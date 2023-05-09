cb5c8ba809974ccb9a60023b7cbd17b1b4a36b15
commandblock2 <tony_hu@live.com>
2023-05-09 11:41:12 +0800

    initialized coffee mod

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Example Mod
-This is an example mod for Kotlin.
+# Coffee Mod
+A fabric implementation of the coffee mod, idea by hsds. The link of the video https://www.bilibili.com/video/BV1JD4y1579K/.
--- coffee-mod-hsds/src/main/kotlin/github/commandblock2/coffee_mod/CoffeeMod.kt
+++ coffee-mod-hsds/src/main/kotlin/github/commandblock2/coffee_mod/CoffeeMod.kt
@@ -1,8 +1,8 @@
-package com.mod_author.mod_id
+package github.commandblock2.coffee_mod
 import net.fabricmc.api.ModInitializer
 @Suppress("UNUSED")
-object ModName: ModInitializer {
-    private const val MOD_ID = "mod_id"
+object CoffeeMod: ModInitializer {
+    private const val MOD_ID = "coffee_mod"
     override fun onInitialize() {
         println("Example mod has been initialized.")
     }
