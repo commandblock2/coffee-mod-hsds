b85d991d1e3e425097bf8aa3fff2b1179b702c57
commandblock2 <tony_hu@live.com>
2023-05-10 13:03:11 +0800

    player doesn't count as sleeping when having the effect

--- coffee-mod-hsds/src/main/kotlin/github/commandblock2/coffee_mod/effect/CoffeeBuzzStatusEffect.kt
+++ coffee-mod-hsds/src/main/kotlin/github/commandblock2/coffee_mod/effect/CoffeeBuzzStatusEffect.kt
@@ -1,10 +1,21 @@
 package github.commandblock2.coffee_mod.effect
 
+import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.attribute.AttributeContainer
 import net.minecraft.entity.effect.StatusEffect
 import net.minecraft.entity.effect.StatusEffectCategory
+import net.minecraft.server.world.ServerWorld
 
 class CoffeeBuzzStatusEffect : StatusEffect(StatusEffectCategory.NEUTRAL, 0x6c4c37) {
     override fun canApplyUpdateEffect(duration: Int, amplifier: Int): Boolean {
         return false
+    }
+
+    override fun onRemoved(entity: LivingEntity?, attributes: AttributeContainer?, amplifier: Int) {
+        super.onRemoved(entity, attributes, amplifier)
+        entity ?: return
+        val world = entity.entityWorld
+        if (world is ServerWorld)
+            world.updateSleepingPlayers()
     }
 }
---
+++ coffee-mod-hsds/src/main/kotlin/github/commandblock2/coffee_mod/mixins/net/minecraft/server/world/MixinSleepManager.java
@@ -0,0 +1,49 @@
+package github.commandblock2.coffee_mod.mixins.net.minecraft.server.world;
+
+import github.commandblock2.coffee_mod.effect.EffectRegistry;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.SleepManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.List;
+
+@Mixin(SleepManager.class)
+public class MixinSleepManager {
+
+    @Shadow
+    private int sleeping;
+
+    @Shadow
+    private int total;
+
+    /**
+     * @author commandblock2
+     * @reason player who has coffee buzz does not count as sleeping
+     */
+    @Overwrite
+    public boolean update(List<ServerPlayerEntity> players) {
+
+
+
+        int updated_total = 0, updated_sleeping = 0;
+        for (final var player: players) {
+            if (player.isSpectator())
+                continue;
+            updated_total++;
+
+            if (!player.isSleeping() || player.hasStatusEffect(EffectRegistry.INSTANCE.getCoffeeBuzzStatusEffect()))
+                continue;
+
+            updated_sleeping++;
+        }
+
+        final boolean updates = !(updated_sleeping <= 0 && sleeping <= 0 || updated_total == total && updated_sleeping == sleeping);
+
+        sleeping = updated_sleeping;
+        total = updated_total;
+
+        return updates;
+    }
+}
