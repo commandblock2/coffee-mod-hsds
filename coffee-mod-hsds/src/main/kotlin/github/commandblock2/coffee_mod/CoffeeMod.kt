67e1e269cd99185524d29d404f6f663a54937b97
commandblock2 <tony_hu@live.com>
2023-05-13 22:27:33 +0800

    clean up

--- coffee-mod-hsds/src/main/kotlin/github/commandblock2/coffee_mod/CoffeeMod.kt
+++ coffee-mod-hsds/src/main/kotlin/github/commandblock2/coffee_mod/CoffeeMod.kt
@@ -24,7 +24,6 @@ import github.commandblock2.coffee_mod.item.CoffeeModItems
 import github.commandblock2.coffee_mod.potion.CoffeeModPotions
 import net.fabricmc.api.ModInitializer
 import net.minecraft.registry.Registry
-import net.minecraft.registry.RegistryKey
 import net.minecraft.util.Identifier
 
 @Suppress("UNUSED")
--- coffee-mod-hsds/src/main/kotlin/github/commandblock2/coffee_mod/entity/effect/CoffeeModEffects.kt
+++ coffee-mod-hsds/src/main/kotlin/github/commandblock2/coffee_mod/entity/effect/CoffeeModEffects.kt
@@ -21,8 +21,6 @@ package github.commandblock2.coffee_mod.entity.effect
 
 import github.commandblock2.coffee_mod.CoffeeMod
 import net.minecraft.registry.Registries
-import net.minecraft.registry.Registry
-import net.minecraft.util.Identifier
 
 object CoffeeModEffects {
      val coffeeBuzzStatusEffect = CoffeeBuzzStatusEffect()
