8fcb05f740b6252bc3fec273dd3e27fc5869eae6
commandblock2 <tony_hu@live.com>
2023-06-18 10:58:21 +0800

    DataGeneration setup and Minecraft 1.20.1!

--- coffee-mod-hsds/build.gradle.kts
+++ coffee-mod-hsds/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.internal.classpath.Instrumented.systemProperty
+
 plugins {
     id("fabric-loom")
     kotlin("jvm").version(System.getProperty("kotlin_version"))
@@ -8,6 +10,19 @@ group = project.extra["maven_group"] as String
 
 loom {
     accessWidenerPath.set(file("src/main/resources/coffee_mod.accesswidener"))
+
+    val datagen by tasks.registering {
+        group = "runs"
+        description = "Data Generation"
+
+        systemProperty("fabric-api.datagen", "true")
+        systemProperty("fabric-api.datagen.output-dir", file("src/main/generated").absolutePath)
+        systemProperty("fabric-api.datagen.modid", project.extra["mod_id"] as String)
+    }
+
+    runs {
+        datagen
+    }
 }
 
 repositories {}
@@ -16,7 +31,11 @@ dependencies {
     mappings("net.fabricmc", "yarn", project.extra["yarn_mappings"] as String, null, "v2")
     modImplementation("net.fabricmc", "fabric-loader", project.extra["loader_version"] as String)
     modImplementation("net.fabricmc.fabric-api", "fabric-api", project.extra["fabric_version"] as String)
-    modImplementation("net.fabricmc", "fabric-language-kotlin", project.extra["fabric_language_kotlin_version"] as String)
+    modImplementation(
+        "net.fabricmc",
+        "fabric-language-kotlin",
+        project.extra["fabric_language_kotlin_version"] as String
+    )
 }
 tasks {
     val javaVersion = JavaVersion.toVersion((project.extra["java_version"] as String).toInt())
@@ -29,7 +48,19 @@ tasks {
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> { kotlinOptions { jvmTarget = javaVersion.toString() } }
     jar { from("LICENSE") { rename { "${it}_${base.archivesName.get()}" } } }
     processResources {
-        filesMatching("fabric.mod.json") { expand(mutableMapOf("version" to project.extra["mod_version"] as String, "fabricloader" to project.extra["loader_version"] as String, "fabric_api" to project.extra["fabric_version"] as String, "fabric_language_kotlin" to project.extra["fabric_language_kotlin_version"] as String, "minecraft" to project.extra["minecraft_version"] as String, "java" to project.extra["java_version"] as String)) }
+        filesMatching("fabric.mod.json") {
+            expand(
+                mutableMapOf(
+                    "version" to project.extra["mod_version"] as String,
+                    "fabricloader" to project.extra["loader_version"] as String,
+                    "fabric_api" to project.extra["fabric_version"] as String,
+                    "fabric_language_kotlin" to project.extra["fabric_language_kotlin_version"] as String,
+                    "minecraft" to project.extra["minecraft_version"] as String,
+                    "java" to project.extra["java_version"] as String,
+                    "mod_id" to project.extra["mod_id"] as String
+                )
+            )
+        }
         filesMatching("*.mixins.json") { expand(mutableMapOf("java" to project.extra["java_version"] as String)) }
     }
     java {
@@ -39,3 +70,4 @@ tasks {
         withSourcesJar()
     }
 }
+sourceSets { main { resources { srcDirs.add(file("src/main/generated")) } } }
---
+++ coffee-mod-hsds/src/main/kotlin/github/commandblock2/coffee_mod/datagen/DataGeneration.kt
@@ -0,0 +1,11 @@
+package github.commandblock2.coffee_mod.datagen
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+@Suppress("unused")
+object DataGeneration : DataGeneratorEntrypoint {
+    override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator?) {
+
+    }
+}
