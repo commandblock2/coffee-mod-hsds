066fbe5fa5be8393d1fb17b32d91c54e826b6550
commandblock2 <tony_hu@live.com>
2023-06-24 00:52:24 +0800

    license*, guess I 'll need to setup something for this

--- coffee-mod-hsds/src/main/kotlin/github/commandblock2/coffee_mod/entity/ai/brain/CoffeeModSchedule.kt
+++ coffee-mod-hsds/src/main/kotlin/github/commandblock2/coffee_mod/entity/ai/brain/CoffeeModSchedule.kt
@@ -24,13 +24,20 @@ import net.minecraft.entity.ai.brain.Schedule
 
 object CoffeeModSchedule {
     val VILLAGER_BABY_COFFEE =
-        Schedule.register("villager_baby_coffee").withActivity(10, Activity.IDLE).withActivity(3000, Activity.PLAY)
-            .withActivity(6000, Activity.IDLE).withActivity(10000, Activity.PLAY).build()
+        Schedule.register("villager_baby_coffee")
+            .withActivity(10, Activity.IDLE)
+            .withActivity(3000, Activity.PLAY)
+            .withActivity(6000, Activity.IDLE)
+            .withActivity(10000, Activity.PLAY).build()
 
 
     val VILLAGER_COFFEE =
-        Schedule.register("villager_coffee").withActivity(10, Activity.IDLE).withActivity(2000, Activity.WORK)
-            .withActivity(9000, Activity.MEET).withActivity(11000, Activity.IDLE).withActivity(12000, Activity.WORK)
+        Schedule.register("villager_coffee")
+            .withActivity(10, Activity.IDLE)
+            .withActivity(2000, Activity.WORK)
+            .withActivity(9000, Activity.MEET)
+            .withActivity(11000, Activity.IDLE)
+            .withActivity(12000, Activity.WORK)
             .build()
     init {
         VILLAGER_COFFEE
